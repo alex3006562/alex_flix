1d19f483dddf3429a3f137844fb69b5763592c22
alex3006562 <Alexsander.Andreata@escola.pr.gov.br>
2025-09-12 11:00:16 -0300

    ok

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-const avanca=document.querySelection
+const avanca=document.querySelectorAll('btn-proximo');
+const reiniciarBtn=document.getElementById('btn-reiniciar');
+
+
+avanca.forEach(button =>) {
+    button.addEventListener('click', function (){
+        const atual=document.querySelector('.ativo'); 
+        const proximoPasso='passo-'+this.getAttribute('data-proximo');
+
+        atual.classList.remove('ativo');
+        const proximoElrmento=document.getElementById(proximoPasso);
+
+        if(proximoElemento){
+            proximoElrmento.classList
+
+}
